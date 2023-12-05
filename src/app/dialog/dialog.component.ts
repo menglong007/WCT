2d08056c5ca301a22b4382dc133ffaabd362f4fc
Your Name <you@example.com>
2023-12-05 17:06:44 +0700

    already

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,72 @@
+import {Component} from "@angular/core";
+import {NgbCarouselConfig} from "@ng-bootstrap/ng-bootstrap";
+import {ThemePalette} from "@angular/material/core";
+import {FormControl} from "@angular/forms";
+
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: 'dialog.component.html'
+})
+export class DialogComponent {
+
+  fontStyleControl = new FormControl('');
+  fontStyle?: string;
+
+  showArrows = true;
+  showIndicators = true;
+  Space : any[] = [];
+
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+    this.Space = [{
+      "display" : "A",
+      "storage" : "B",
+      "camera" : "C",
+      "battery" : "D",
+      "chip" : "E",
+      "Released" : "F",
+      "weight": "G"
+    }]
+  }
+
+
+
+  slides = [
+    {
+      imageUrl: 'https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg',
+      caption: 'beach'
+    },
+    {
+      imageUrl: 'https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg',
+      caption: 'beach'
+    },
+    {
+      imageUrl: 'https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg',
+      caption: 'beach'
+    },
+    {
+      imageUrl: 'https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg',
+      caption: 'beach'
+    },
+
+  ];
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+  selectedFood = this.foods[2].value;
+
+  icons = ['crop_square', 'storage', 'camera', 'battery_full', 'memory', 'calendar_month', 'smartphone'];
+
+
+}
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,84 @@
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {MatDialog} from "@angular/material/dialog";
+import {DialogComponent} from "../dialog/dialog.component";
+
+
+@Component({
+  selector: 'app-item',
+  templateUrl: 'item.component.html'
+
+})
+export class ItemComponent {
+  @Input() selected: any;
+  @Output() selectedChange = new EventEmitter<boolean>();
+
+  public toggleSelected() {
+    this.selected = !this.selected;
+    this.selectedChange.emit(this.selected);
+  }
+  product: any[] = [];
+
+  constructor(public dialog: MatDialog) {
+    this.product = [{
+      "name": "iphone",
+      "items": [{
+        "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+        "model": "IPX",
+        "price_off" : "1500$",
+        "price" : "1400$"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX",
+          "price_off" : "1500$",
+          "price" : "1400$"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX",
+          "price" : "1400$"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX"
+        },
+        {
+          "image": "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-230912_inline.jpg.large.jpg",
+          "model": "IPX"
+        },
+
+
+
+      ]
+    },
+    ]
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      height: '707px',
+      width: '1000px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+}
